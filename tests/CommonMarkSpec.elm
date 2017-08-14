33f5129a2849f21046a763834818d6e60846287a
Brian Hicks <brian@brianthicks.com>
2017-08-14 12:06:02 -0500

    tests: change to longer names in thematic breaks

--- tests/CommonMarkSpec.elm
+++ tests/CommonMarkSpec.elm
@@ -27,15 +27,15 @@ group description tester candidates =
 thematicBreak : Test
 thematicBreak =
     let
-        isOnly : String -> () -> Expectation
-        isOnly raw unitToSatisfyTestType =
+        isOnlyThematicBreak : String -> () -> Expectation
+        isOnlyThematicBreak raw unitToSatisfyTestType =
             -- reversed because the ordering is built for pipelines
             Expect.equal
                 (Ok [ ThematicBreak ])
                 (parseString raw)
 
-        isNot : String -> () -> Expectation
-        isNot raw unitToSatisfyTestType =
+        isNotThematicBreak : String -> () -> Expectation
+        isNotThematicBreak raw unitToSatisfyTestType =
             case parseString raw |> Result.map List.head of
                 Ok (Just ThematicBreak) ->
                     Expect.fail "got a thematic break"
@@ -51,8 +51,8 @@ thematicBreak =
                     -- implemented. Should it fail with the error?
                     Expect.pass
 
-        contains : String -> () -> Expectation
-        contains raw unitToSatisfyTestType =
+        containsThematicBreak : String -> () -> Expectation
+        containsThematicBreak raw unitToSatisfyTestType =
             -- TODO: revisit this once other block-level elements are
             -- implemented. Right now it's an error since only thematic breaks
             -- are implemented.
@@ -60,51 +60,51 @@ thematicBreak =
     in
     describe "thematic breaks"
         [ group "valid characters"
-            isOnly
+            isOnlyThematicBreak
             [ "***", "---", "___" ]
         , group "wrong characters"
-            isNot
+            isNotThematicBreak
             [ "+++", "===" ]
         , group "not enough characters"
-            isNot
+            isNotThematicBreak
             [ "--", "**", "__" ]
         , group "one to three spaces indent are allowed"
-            isOnly
+            isOnlyThematicBreak
             [ " ***", "  ***", "   ***" ]
         , group "four spaces is too many"
-            isNot
+            isNotThematicBreak
             [ "    ***", "Foo\n    ***" ]
         , group "more than three characters may be used"
-            isOnly
+            isOnlyThematicBreak
             [ "_____________________________________" ]
         , group "spaces are allowed between the characters"
-            isOnly
+            isOnlyThematicBreak
             [ " - - -"
             , " **  * ** * ** * **"
             , "-     -     -     -"
             ]
         , group "spaces are allowed at the end"
-            isOnly
+            isOnlyThematicBreak
             [ "- - - -    " ]
         , group "no other characters may occur in the line"
-            isNot
+            isNotThematicBreak
             [ "_ _ _ _ a"
             , "a------"
             , "---a---"
             ]
         , group "it is required that all of the non-whitespace characters be the same"
-            isNot
+            isNotThematicBreak
             [ " *-*" ]
         , group "thematic breaks do not need blank lines before or after"
-            contains
+            containsThematicBreak
             [ "- foo\n***\n- bar" ]
         , group "when both a thematic break and a setext heading are possible, the setext heading takes precedence"
-            isNot
+            isNotThematicBreak
             [ "Foo\n---\nbar" ]
         , group "when both a thematic break and a list item are possible, the thematic break takes precedence"
-            contains
+            containsThematicBreak
             [ "* Foo\n* * *\n* Bar" ]
         , group "if you want a thematic break in a list item, use a different bullet"
-            contains
+            containsThematicBreak
             [ "- Foo\n- * * *" ]
         ]
